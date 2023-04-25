5004062d35b94c6ad2f337e84401c03e9aca8477
Anastasiya Nikalayeva <nikalayeva@gmail.com>
2023-04-25 14:02:41 +0200

    Change ambientLight intensity

--- initial.js
+++ initial.js
@@ -163,7 +163,7 @@ window.onload = function () {
     /**
      * Lights
      */
-    const ambientLight = new THREE.AmbientLight(0xffffff, 0.7)
+    const ambientLight = new THREE.AmbientLight(0xffffff, 1)
     scene.add(ambientLight)
 
     const directionalLight = new THREE.DirectionalLight(0xffffff, 0.2)
