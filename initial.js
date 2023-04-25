bbdd4f06ce51ef4f1f556e0eca0582471789ae84
Anastasiya Nikalayeva <nikalayeva@gmail.com>
2023-04-25 13:48:52 +0200

    Implement hover (on boxes) events

--- initial.js
+++ initial.js
@@ -95,213 +95,240 @@ window.onload = function () {
     // await loadImages(imagesSrc)
 
 
-      /**
-   * Three.js and Cannon.js
-   */
-  // Canvas
-  const canvas = document.querySelector('canvas.webgl')
+    /**
+     * Three.js and Cannon.js
+     */
+    // Canvas
+    const canvas = document.querySelector('canvas.webgl')
 
-  // Scene
-  const scene = new THREE.Scene()
-  
-  /**
-   * Textures
-   */
-  const textureLoader = new THREE.TextureLoader()
-  const cubeTextureLoader = new THREE.CubeTextureLoader()
+    // Scene
+    const scene = new THREE.Scene()
 
-  const environmentMapTexture = cubeTextureLoader.load([
-    '/textures/environmentMaps/1/px.png',
-    '/textures/environmentMaps/1/nx.png',
-    '/textures/environmentMaps/1/py.png',
-    '/textures/environmentMaps/1/ny.png',
-    '/textures/environmentMaps/1/pz.png',
-    '/textures/environmentMaps/1/nz.png'
-  ])
+    /**
+     * Textures
+     */
+    const textureLoader = new THREE.TextureLoader()
+    const cubeTextureLoader = new THREE.CubeTextureLoader()
 
-  /**
-   * Physics
-   */
+    const environmentMapTexture = cubeTextureLoader.load([
+      '/textures/environmentMaps/1/px.png',
+      '/textures/environmentMaps/1/nx.png',
+      '/textures/environmentMaps/1/py.png',
+      '/textures/environmentMaps/1/ny.png',
+      '/textures/environmentMaps/1/pz.png',
+      '/textures/environmentMaps/1/nz.png'
+    ])
 
-  // World
-  const world = new CANNON.World()
-  world.broadphase = new CANNON.SAPBroadphase(world)
-  world.allowSleep = true
-  world.gravity.set(0, -9.82, 0)
+    /**
+     * Physics
+     */
 
-  // Materials
-  const defaultMaterial = new CANNON.Material('default')
+    // World
+    const world = new CANNON.World()
+    world.broadphase = new CANNON.SAPBroadphase(world)
+    world.allowSleep = true
+    world.gravity.set(0, -9.82, 0)
 
-  const defaultContactMaterial = new CANNON.ContactMaterial(
-    defaultMaterial,
-    defaultMaterial,
-    {
-      friction: 0.2,
-      restitution: 0.1
+    // Materials
+    const defaultMaterial = new CANNON.Material('default')
+
+    const defaultContactMaterial = new CANNON.ContactMaterial(
+      defaultMaterial,
+      defaultMaterial,
+      {
+        friction: 0.2,
+        restitution: 0.1
+      }
+    )
+    world.addContactMaterial(defaultContactMaterial)
+    world.defaultContactMaterial = defaultContactMaterial
+
+
+    // Floor
+    const floorShape = new CANNON.Plane()
+    const floorBody = new CANNON.Body()
+    floorBody.mass = 0
+    floorBody.addShape(floorShape)
+    floorBody.quaternion.setFromAxisAngle(
+      new CANNON.Vec3(-1, 0, 0),
+      Math.PI * 0.5
+    )
+    world.addBody(floorBody)
+
+    /**
+     * Floor
+     */
+    const floor = new THREE.Mesh(
+      new THREE.PlaneGeometry(10, 10),
+      new THREE.MeshStandardMaterial({
+        color: '#777777',
+        metalness: 0.3,
+        roughness: 0.4,
+        envMap: environmentMapTexture,
+        envMapIntensity: 0.5
+      })
+    )
+    floor.receiveShadow = true
+    floor.rotation.x = - Math.PI * 0.5
+    scene.add(floor)
+
+    /**
+     * Lights
+     */
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.7)
+    scene.add(ambientLight)
+
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 0.2)
+    directionalLight.castShadow = true
+    directionalLight.shadow.mapSize.set(1024, 1024)
+    directionalLight.shadow.camera.far = 15
+    directionalLight.shadow.camera.left = - 7
+    directionalLight.shadow.camera.top = 7
+    directionalLight.shadow.camera.right = 7
+    directionalLight.shadow.camera.bottom = - 7
+    directionalLight.position.set(5, 5, 5)
+    scene.add(directionalLight)
+
+    /**
+     * Sizes
+     */
+    const sizes = {
+      width: 300,
+      height: 600
     }
-  )
-  world.addContactMaterial(defaultContactMaterial)
-  world.defaultContactMaterial = defaultContactMaterial
 
+    /**
+     * Raycaster
+     */
+    const raycaster = new THREE.Raycaster()
 
-  // Floor
-  const floorShape = new CANNON.Plane()
-  const floorBody = new CANNON.Body()
-  floorBody.mass = 0
-  floorBody.addShape(floorShape)
-  floorBody.quaternion.setFromAxisAngle(
-    new CANNON.Vec3(-1, 0, 0),
-    Math.PI * 0.5
-  )
-  world.addBody(floorBody)
+    /**
+     * Mouse
+     */
+    // const mouse = new THREE.Vector2()
+    let mouse = null
 
-  /**
-   * Floor
-   */
-  const floor = new THREE.Mesh(
-    new THREE.PlaneGeometry(10, 10),
-    new THREE.MeshStandardMaterial({
-      color: '#777777',
-      metalness: 0.3,
-      roughness: 0.4,
-      envMap: environmentMapTexture,
-      envMapIntensity: 0.5
-    })
-  )
-  floor.receiveShadow = true
-  floor.rotation.x = - Math.PI * 0.5
-  scene.add(floor)
-
-  /**
-   * Lights
-   */
-  const ambientLight = new THREE.AmbientLight(0xffffff, 0.7)
-  scene.add(ambientLight)
-
-  const directionalLight = new THREE.DirectionalLight(0xffffff, 0.2)
-  directionalLight.castShadow = true
-  directionalLight.shadow.mapSize.set(1024, 1024)
-  directionalLight.shadow.camera.far = 15
-  directionalLight.shadow.camera.left = - 7
-  directionalLight.shadow.camera.top = 7
-  directionalLight.shadow.camera.right = 7
-  directionalLight.shadow.camera.bottom = - 7
-  directionalLight.position.set(5, 5, 5)
-  scene.add(directionalLight)
-
-  /**
-   * Sizes
-   */
-  const sizes = {
-    width: 300,
-    height: 600
-  }
-
-  /**
-   * Camera
-   */
-  // Base camera
-  const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 15)
-  camera.position.set(0, 3, 7)
-  scene.add(camera)
-
-  /**
-   * Renderer
-   */
-  const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-  })
-  renderer.shadowMap.enabled = true
-  renderer.shadowMap.type = THREE.PCFSoftShadowMap
-  renderer.setSize(sizes.width, sizes.height)
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-  renderer.setClearColor('#262837')
-  /**
-   * Utils
-   */
-  const objectsToUpdate = []
-
-  const boxGeometry = new THREE.BoxGeometry(1, 1, 1)
-  const boxMaterial = new THREE.MeshStandardMaterial({
-    metalness: 0.3,
-    roughness: 0.4,
-    envMap: environmentMapTexture
-  })
-
-  const createBoxes = (width, height, depth, position) => {
-    // Three.js mesh
-    const mesh = new THREE.Mesh(boxGeometry, boxMaterial)
-    mesh.scale.set(width, height, depth)
-    mesh.castShadow = true
-    mesh.position.copy(position)
-    scene.add(mesh)
-
-    // CANNON.js mesh
-    const boxShape = new CANNON.Box(new CANNON.Vec3(width * 0.5, height * 0.5, depth * 0.5))
-    const boxBody = new CANNON.Body({
-      mass: 1,
-      position: new CANNON.Vec3(0, 3, 0),
-      shape: boxShape,
-      material: defaultMaterial
-    })
-    boxBody.position.copy(mesh.position)
-    world.addBody(boxBody)
-
-    // Save in objects to update
-    objectsToUpdate.push({
-      mesh: mesh,
-      body: boxBody
+    document.addEventListener('mousemove', (event) => {
+      mouse = new THREE.Vector2()
+      mouse.x = event.clientX / sizes.width * 2 - 1
+      mouse.y = -(event.clientY / sizes.height * 2 - 1)
     })
 
-  }
+    /**
+     * Camera
+     */
+    // Base camera
+    const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 15)
+    camera.position.set(0, 3, 7)
+    scene.add(camera)
 
-  const createBoxesToCall = () => {
-    const random = Math.random() + 0.5
-    createBoxes(
+    /**
+     * Renderer
+     */
+    const renderer = new THREE.WebGLRenderer({
+      canvas: canvas
+    })
+    renderer.shadowMap.enabled = true
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+    renderer.setClearColor('#262837')
+    /**
+     * Utils
+     */
+    const objectsToUpdate = []
+    const objectsToTest = []
+
+    const boxGeometry = new THREE.BoxGeometry(1, 1, 1)
+    const createBoxes = (width, height, depth, position) => {
+
+      // Three.js mesh
+      const mesh = new THREE.Mesh(boxGeometry, new THREE.MeshStandardMaterial({
+        color: 0xffffff,
+        metalness: 0.3,
+        roughness: 0.4,
+        envMap: environmentMapTexture
+      }))
+      mesh.scale.set(width, height, depth)
+      mesh.castShadow = true
+      mesh.position.copy(position)
+      scene.add(mesh)
+
+      // CANNON.js mesh
+      const boxShape = new CANNON.Box(new CANNON.Vec3(width * 0.5, height * 0.5, depth * 0.5))
+      const boxBody = new CANNON.Body({
+        mass: 1,
+        position: new CANNON.Vec3(0, 3, 0),
+        shape: boxShape,
+        material: defaultMaterial
+      })
+      boxBody.position.copy(mesh.position)
+      world.addBody(boxBody)
+
+      // Save in objects to update
+      objectsToUpdate.push({
+        mesh: mesh,
+        body: boxBody
+      })
+      objectsToTest.push(mesh)
+    }
+
+    const createBoxesToCall = () => {
+      const random = Math.random() + 0.5
+      createBoxes(
         random * 0.5,
         random * 0.5,
         random * 0.5,
         {
-            x: 0,
-            y: 2,
-            z: Math.random() - 0.5 * 3
+          x: 0,
+          y: 2,
+          z: Math.random() - 0.5 * 3
         }
-    )
-}
-
-const buttonToCreateBoxes = document.querySelector('#button')
-buttonToCreateBoxes.addEventListener('click', createBoxesToCall)
-  /**
-   * Animate
-   */
-  const clock = new THREE.Clock()
-  let oldElapsedTime = 0
-
-  const tick = () => {
-    const elapsedTime = clock.getElapsedTime()
-    const deltaTime = elapsedTime - oldElapsedTime
-    oldElapsedTime = elapsedTime
-
-    // Update physics world
-    world.step(1 / 60, deltaTime, 3)
-
-    for (const object of objectsToUpdate) {
-      object.mesh.position.copy(object.body.position)
-      object.mesh.quaternion.copy(object.body.quaternion)
+      )
     }
 
+    const buttonToCreateBoxes = document.querySelector('#button')
+    buttonToCreateBoxes.addEventListener('click', createBoxesToCall)
+    /**
+     * Animate
+     */
+    const clock = new THREE.Clock()
+    let oldElapsedTime = 0
 
-    // Update controls
-    // controls.update()
+    const tick = () => {
+      const elapsedTime = clock.getElapsedTime()
+      const deltaTime = elapsedTime - oldElapsedTime
+      oldElapsedTime = elapsedTime
 
-    // Render
-    renderer.render(scene, camera)
+      // Update physics world
+      world.step(1 / 60, deltaTime, 3)
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
-  }
+      for (const object of objectsToUpdate) {
+        object.mesh.position.copy(object.body.position)
+        object.mesh.quaternion.copy(object.body.quaternion)
+      }
 
-  tick()
+      // Cast a ray
+      if (mouse) {
+        raycaster.setFromCamera(mouse, camera)
+      }
+      const intersects = raycaster.intersectObjects(objectsToTest)
+      for (const object of objectsToTest) {
+        object.material.color.set('#ffffff')
+      }
+
+      if (intersects.length) {
+        intersects[0].object.material.color.set('#22bdef')
+      }
+
+      // Render
+      renderer.render(scene, camera)
+
+      // Call tick again on the next frame
+      window.requestAnimationFrame(tick)
+    }
+
+    tick()
 
     /**
      * Animating
@@ -310,7 +337,7 @@ buttonToCreateBoxes.addEventListener('click', createBoxesToCall)
 
     const tl = gsap.timeline()
     tl
-    .to(wrect, { duration: 1, alpha: 0, ease: "none" })
+      .to(wrect, { duration: 1, alpha: 0, ease: "none" })
 
   }
 }
